d93c937a8eab3aeaf39d63142fa9e3df69947e5f
Rp70 <14834373+Rp70@users.noreply.github.com>
2025-01-20 12:12:33 -0800

    refactor: check for filename length

--- exercise1.c
+++ exercise1.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
  
 #define BUFSIZE 256
-    
+#define MAX_ADDRESS_LENGTH 260 // This length is for Windows, but I think that's okay with Linux too
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
+    }
+    if (strlen(argv[1]) > MAX_ADDRESS_LENGTH) {
+        fprintf(stderr, "File path is too long!\n");
+        return -2;
     }
     char cmd[BUFSIZE] = "wc -c < ";
     strcat(cmd, argv[1]);
